312aa38820744929f4c6a58a6f7e33a3c337bd7f
Piotr <piotr.bania@europe.com>
2022-08-09 08:08:17 +0100

    init

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Slider from '../components/Slider'
+import Slider from '../components/slider'
 
 export default function Home() {
 return (
