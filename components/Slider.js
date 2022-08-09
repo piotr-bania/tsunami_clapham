03aaca43698fd6a147ad0b4ed8c6ae986b7f408b
Piotr <piotr.bania@europe.com>
2022-08-09 08:41:33 +0100

    init

--- components/Slider.js
+++ components/Slider.js
@@ -1,27 +1,54 @@
-import { Canvas, extend } from '@react-three/fiber'
+import * as THREE from 'three'
+import React, { useRef, Suspense } from 'react'
+import { Canvas, extend, useFrame } from '@react-three/fiber'
 import { shaderMaterial } from '@react-three/drei'
 import glsl from 'babel-plugin-glsl/macro'
 
 const WaveShaderMaterial = shaderMaterial(
-  // Uniform
-{},
+// Uniform
+{
+uTime: 0,
+uColor: new THREE.Color(0.0, 1.0, 0.0)
+},
 
 // Vertex shader
 glsl`
-  void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }
+varying vec2 vUv;
+
+void main() {
+vUv = uv;
+
+gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
 `,
 
 // Fragment shader
 glsl`
+precision mediump float;
+
+uniform vec3 uColor;
+uniform float uTime;
+
+varying vec2 vUv;
+
 void main() {
-  gl_FragColor = vec4(0.0, 0.4, 1.0, 1.0);
+gl_FragColor = vec4(sin(vUv.x * uTime) * uColor, 1.0);
 }
 `
 )
 
 extend({ WaveShaderMaterial })
+
+const Wave = () => {
+const ref = useRef()
+useFrame(({clock}) => (ref.current.uTime = clock.getElapsedTime()))
+return (
+<mesh>
+  <planeBufferGeometry args={[3, 5]} />
+  <waveShaderMaterial uColor={"pink"} ref={ref} />
+</mesh>
+)
+}
 
 const Slider = () => {
 return (
@@ -37,10 +64,9 @@ return (
   <pointLight color="#D0D0D1" intensity={0.5} position={[-2, 1, 2]} />
   <directionalLight color="#697077" intensity={0.5} position={[-5, 0, 0]} />
 
-  <mesh>
-    <planeBufferGeometry args={[3, 5]} />
-    <waveShaderMaterial color="red" />
-  </mesh>
+  <Suspense fallback={null}>
+    <Wave />
+  </Suspense>
 
 </Canvas>
 )
